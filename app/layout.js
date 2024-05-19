742bf29eb5e4e5ff5e15ef187fb6bb7cf7630019
Debjit Majumder <majumderdebjit@gmail.com>
2024-05-19 08:12:19 +0530

    add register page

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Montserrat } from "next/font/google";
 import "../styles/global.css";
 
 export const metadata = {
@@ -5,9 +6,14 @@ export const metadata = {
   description: "DeusGPT Application",
 };
 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "900"],
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={montserrat.className}>
       <body className="bg-[#ccddec] h-[100vh]">
         {/* Layout UI */}
         {children}
--- components/Register/index.js
+++ components/Register/index.js
@@ -10,17 +10,20 @@ const Input = ({ onChange, name, ...rest }) => {
       name={name}
       onChange={(e) => onChange(name, e.target.value)}
       className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 
-          text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground 
-          focus:border-ring focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 focus-visible:ring-0 
-          focus-visible:border-ring"
+        text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground 
+        focus:border-ring focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 focus-visible:ring-0 
+        focus-visible:border-ring"
     />
   );
 };
 
-const Login = () => {
+const Register = () => {
   const [values, setValues] = useState({
+    name: "",
+    lastName: "",
     email: "",
     password: "",
+    confirmPassword: "",
   });
 
   const onChange = (name, value) => {
@@ -46,7 +49,7 @@ const Login = () => {
       </div>
       <div className="mt-[30px]">
         <div className="text-center font-medium">
-          Create an account or log in
+          Create an account and log in
         </div>
 
         <div className="grid gap-6 mt-[6px]">
@@ -54,8 +57,26 @@ const Login = () => {
             <div className="grid gap-2">
               <div className="grid">
                 <Input
+                  name="name"
+                  placeholder="Name"
+                  onChange={onChange}
+                  value={values.name}
+                  type="text"
+                />
+              </div>
+              <div className="grid">
+                <Input
+                  name="lastName"
+                  placeholder="Last Name"
+                  onChange={onChange}
+                  value={values.lastName}
+                  type="text"
+                />
+              </div>
+              <div className="grid">
+                <Input
                   name="email"
-                  placeholder="name@example.com"
+                  placeholder="Email"
                   onChange={onChange}
                   value={values.email}
                   type="email"
@@ -64,11 +85,35 @@ const Login = () => {
               <div className="grid">
                 <Input
                   name="password"
-                  placeholder="password"
+                  placeholder="Password"
                   onChange={onChange}
                   value={values.password}
                   type="password"
                 />
+              </div>
+              <div className="grid">
+                <Input
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  onChange={onChange}
+                  value={values.confirmPassword}
+                  type="password"
+                />
+              </div>
+              <div className="flex gap-4 h-[50px] items-center justify-center">
+                <input
+                  type="checkbox"
+                  checked={values.termsAndConditions}
+                  onChange={(e) =>
+                    onChange("termsAndConditions", e.target.checked)
+                  }
+                  id="termsAndConditions"
+                  name="termsAndConditions"
+                  className="cursor-pointer"
+                />
+                <label className="cursor-pointer" htmlFor="termsAndConditions">
+                  I accept the rules
+                </label>
               </div>
 
               <button
@@ -76,29 +121,16 @@ const Login = () => {
                 rounded-md text-sm font-medium ring-offset-background transition-colors 
                 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 
                 border border-input dark:border-gray-500 bg-background hover:bg-[#f3f3f2] 
-                h-10 px-4 py-2"
+                h-10 px-4 py-2 mt-[10px]"
               >
-                Login
+                Register
               </button>
             </div>
           </form>
-
-          <button className="border-none outline-none text-[17px] font-[400]">
-            I forgot my password?
-          </button>
-
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t"></span>
-            </div>
-            <div className="relative z-10 flex justify-center text-xs uppercase">
-              <span className="bg-white px-2 text-[#766f6b]">Or continue</span>
-            </div>
-          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
