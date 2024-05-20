f2262f3045131ca7dbcd0cf0767bf509076874cd
Soham <rockstarsam1234@gmail.com>
2024-05-21 00:11:28 +0530

    Updated pricing page

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -14,47 +14,39 @@ const Pricing = () => {
   return (
     <div className="!inset-0 d-flex flex-col font-montserrat selection:bg-selection bg-background text-foreground">
       <LandingPageNavbar shortIcon={true} />
-      <main className="d-flex grow overflow-visible grid justify-items-stretch mb-5 md:grid-cols-3 md:justify-center md:items-start md:justify-items-center">
-        <div className="d-flex flex-row items-center mb-16 md:mb-0 z-10 justify-center overflow-hidden">
-          <div className="d-flex flex-row md:flex-row md:relative bg-background z-10 gap-6">
-            <div
-              className="relative z-10 px-6 md:px-0 font-inter"
-              role="region"
-              aria-roledescription="carousel"
-            >
-              <div className="overflow-hidden">
-                <div className="d-flex justify-between flex-row">
-                  {pricingOptions.map(
-                    (
-                      {
-                        heading,
-                        description,
-                        planId,
-                        originalPrice,
-                        discountedPrice,
-                        popular,
-                        features,
-                      },
-                      index
-                    ) => (
-                      <PriceCard
-                        heading={heading}
-                        description={description}
-                        planId={planId}
-                        originalPrice={originalPrice}
-                        discountedPrice={discountedPrice}
-                        popular={popular}
-                        features={features}
-                        index={index}
-                        key={index}
-                      />
-                    )
-                  )}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <main
+        style={{
+          gap: "30px",
+          marginTop: "30px",
+        }}
+        className="d-flex grow overflow-visible grid justify-center align-center justify-items-stretch mb-5 md:grid-cols-3 md:justify-center md:items-start md:justify-items-center"
+      >
+        {pricingOptions.map(
+          (
+            {
+              heading,
+              description,
+              planId,
+              originalPrice,
+              discountedPrice,
+              popular,
+              features,
+            },
+            index
+          ) => (
+            <PriceCard
+              heading={heading}
+              description={description}
+              planId={planId}
+              originalPrice={originalPrice}
+              discountedPrice={discountedPrice}
+              popular={popular}
+              features={features}
+              index={index}
+              key={index}
+            />
+          )
+        )}
       </main>
       <div style={{ position: "absolute", bottom: 0, left: 0, width: "100%" }}>
         <Footer />
--- components/PriceCard/PriceCard.js
+++ components/PriceCard/PriceCard.js
@@ -8,30 +8,6 @@ import { idTokenAtom, loaderAtom } from "../../store";
 import { useRouter } from "next/navigation";
 import { initiatePayment } from "../../utils";
 
-const Feature = ({ description, index }) => {
-  return (
-    <li className={styles.feature} key={index}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="18"
-        height="17"
-        viewBox="0 0 18 17"
-        fill="none"
-        className={styles.tick_icon}
-      >
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M16.1242 1.27115L5.38112 11.6391L2.53032 8.5932C2.00517 8.09806 1.17994 8.06806 0.579775 8.48818C-0.0053927 8.9233 -0.17044 9.6885 0.189661 10.3036L3.56561 15.7952C3.8957 16.3054 4.46586 16.6204 5.11104 16.6204C5.72621 16.6204 6.31138 16.3054 6.64147 15.7952C7.18163 15.09 17.4895 2.80158 17.4895 2.80158C18.8399 1.42119 17.2045 0.205847 16.1242 1.25614V1.27115Z"
-          fill="#006AFF"
-        ></path>
-      </svg>
-
-      <p className={styles.feature_description}>{description}</p>
-    </li>
-  );
-};
-
 const PriceCard = ({
   heading,
   planId,
@@ -59,39 +35,49 @@ const PriceCard = ({
   };
 
   return (
-    <div className={`${styles.container}`} key={index}>
-      <div>
-        {popular && <span className={styles.popular_pack}>Most Popular</span>}
-        <h3 className={styles.heading_title}> {heading} </h3>
-        <p className={styles.heading_description}>{description}</p>
-        <span>
-          <h1 className={styles.price}>
-            {discountedPrice}
-            <span className={styles.price_actual_container}>
-              <del className={styles.price_actual_del}>${originalPrice}</del>
-              <span>/month</span>
-            </span>
-          </h1>
-        </span>
-        <p className={styles.key_points_container}>
-          <span className={styles.key_point}>
-            <b>80000</b>
-            words{" "}
-          </span>
-          <span>
-            <b>6000</b> images
-          </span>
-        </p>
-      </div>
-      <button className={styles.upgrade_btn} onClick={handleClick}>
-        Purchase now
-      </button>
-      <div>
-        <ul className={styles.feature_list}>
-          {features.map((description, index) => (
-            <Feature key={index} description={description} index={index} />
-          ))}
-        </ul>
+    <div>
+      <div
+        className={`${styles.card} ${styles.price_card} ${
+          popular && styles.popular
+        }`}
+      >
+        <div className={styles.card_body}>
+          <div
+            className={`${styles.price_head} ${
+              popular && styles.popular_price_head
+            }`}
+          >
+            {popular && (
+              <span className="badge f-12 bg-success mb-3"> Popular </span>
+            )}
+            <h5 className="mb-0">{heading}</h5>
+
+            <div className={`${styles.price} mt-4`}>
+              {`$${discountedPrice}`}
+              <span className={styles.text_muted}>/month</span>
+            </div>
+
+            <div className={styles.d_grid}>
+              <a
+                style={{
+                  borderRadius: "20px",
+                  textDecoration: "none",
+                }}
+                className="btn btn-primary mt-4"
+              >
+                Buy Now
+              </a>
+            </div>
+          </div>
+
+          <ul className={`list-group ${styles.product_list}`}>
+            {features.map((feature, index) => (
+              <li key={index} className={styles.feature}>
+                {feature}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
