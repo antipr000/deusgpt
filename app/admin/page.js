d1b2619863fcfe2d0b14ec150e5ecb76c662bf42
Soham <rockstarsam1234@gmail.com>
2024-05-26 21:50:16 +0530

    Fixed few issues in admin page

--- app/admin/page.js
+++ app/admin/page.js
@@ -5,6 +5,43 @@ import { useRouter } from "next/navigation";
 import { useAtom, useAtomValue } from "jotai";
 import { getUser } from "../../api";
 import { idTokenAtom, loaderAtom, userAtom } from "../../store";
+import { store } from "../../store/store";
+
+const handleEvent = ({ data }) => {
+  const { type, payload } = data;
+  const idToken = store.get(idTokenAtom);
+  const user = store.get(userAtom);
+  const iframeRef = document.getElementById("lobechat");
+  console.log("Received event", type, payload);
+  switch (type) {
+    case "admin-load":
+      store.set(loaderAtom, (_) => ({
+        show: false,
+        message: null,
+      }));
+      if (payload) {
+        iframeRef.contentWindow.postMessage(
+          {
+            type: "id-token",
+            payload: {
+              idToken,
+              user,
+            },
+          },
+          "*"
+        );
+      }
+      break;
+    case "signout":
+      if (payload) {
+        firebaseSignOut();
+      }
+    case "open-link":
+      if (payload) {
+        window.open(payload, "_blank");
+      }
+  }
+};
 
 const AdminPage = () => {
   const iframeRef = useRef(null);
@@ -12,6 +49,10 @@ const AdminPage = () => {
   const idToken = useAtomValue(idTokenAtom);
   const [_, setLoader] = useAtom(loaderAtom);
   const router = useRouter();
+
+  useEffect(() => {
+    window.addEventListener("message", handleEvent);
+  }, []);
 
   useEffect(() => {
     console.log("IDTOKEN", idToken);
